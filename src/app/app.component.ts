3b492843eb0597ad5635d1cd663aeebcd8c882d8
Kadir Bakır <starmagekadir531@gmail.com>
2023-08-09 23:48:21 +0300

    Initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,38 @@
+import { Component } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'Template form';
+  frm: FormGroup;
+  
+  constructor(private formbuilder: FormBuilder) {
+    this.frm = formbuilder.group({
+      name: [""],
+      surname: [""],
+      eposta: [""],
+      telefon: [""],
+      DTarihi: [""]
+    });
+  }
+  FormData:string;
+  Sand(data: {
+    name: string,
+    surname: string,
+    eposta: string,
+    telefon: string,
+    DTarihi: string
+  }) {
+    var datas=document.querySelector("#p");
+    for (const key in data) {
+     datas.innerHTML +=`
+     <p class="mb-1"> ${data[key]} </p>
+     `;
+    }
+   
+  }
+}
